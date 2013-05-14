33266194d2d175da13091efc39c0edc0c3d648ca
Azat Abdulvaliev <atik1990@ya.ru>
2013-05-14 14:14:51 +0400

    Renaming tool, number of frames as parameter, hint for parameters

--- src/histmapred/VideoHistogramComputer.java
+++ src/histmapred/VideoHistogramComputer.java
@@ -1,26 +1,32 @@
 package histmapred;
 
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
 
 /**
  * User: azat, Date: 13.05.13, Time: 1:32
  */
-public class SmallFilesToSequenceFileConverter extends Configured implements Tool {
+public class VideoHistogramComputer extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws IOException {
+        if (args.length != 3) {
+            System.out.println("arguments: <input path> <output path> <number of frames>");
+            return -1;
+        }
         JobConf conf = new JobConf(getConf(), getClass());
         if (conf == null) {
             return -1;
         }
         FileInputFormat.addInputPath(conf, new Path(args[0]));
         FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+        conf.set("frames", args[2]);
 
         conf.setInputFormat(CombineWholeFileInputFormat.class);
         conf.setOutputFormat(TextOutputFormat.class);
@@ -37,7 +43,7 @@ public class SmallFilesToSequenceFileConverter extends Configured implements Too
     }
 
     public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new SmallFilesToSequenceFileConverter(), args);
+        int exitCode = ToolRunner.run(new VideoHistogramComputer(), args);
         System.exit(exitCode);
     }
 }
